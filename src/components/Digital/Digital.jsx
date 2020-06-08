e44b53410502b76493e641fd89fc82864844d7ff
Jack Gay <jackpowis@gmail.com>
2020-06-08 11:05:07 +0100

    commit: creating components

---
+++ src/components/Digital/Digital.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Digital extends Component {
+    render() {
+        return (
+            <div className="digital">
+                
+            </div>
+        );
+    }
+}
+
+export default Digital;
---
+++ src/components/Print/Print.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Print extends Component {
+    render() {
+        return (
+            <div className="print">
+                
+            </div>
+        );
+    }
+}
+
+export default Print;
