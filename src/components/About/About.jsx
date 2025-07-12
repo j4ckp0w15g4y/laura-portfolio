6779a5ab2c325465f4a434130bceaaff7ab4c6d6
Jack Gay <jackpowis@gmail.com>
2025-07-12 12:00:24 +0200

    remove laura bio

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -12,8 +12,6 @@ class About extends Component {
                    <span className="laura-name"> Laura Isabella</span>
                     <br />
                     Freelance Writer and Digital Editor
-                    <br />
-                    Brand Consultant / Special Projects
                 </p>
 
                 <p className="left-text-box">
@@ -21,12 +19,6 @@ class About extends Component {
                     <br />                    
                     lauraisabella.co.uk
                 </p>
-
-                <div className="bio">
-                    <p>A highly skilled writer, digital editor, and brand consultant; Laura Isabella has been working within the creative industries with a key focus on fashion, youth culture and championing emerging talent for the past eight years. Laura has worked both freelance and in-house for a plethora of titles, with roles including Online Editor at <span className="brand-italic">Wonderland Magazine</span> and Social Media Manager at <span className="brand-italic">The Business of Fashion</span> and <span className="brand-italic">Hunger TV</span>. She has a keen interest in working creatively on bespoke advertorial content and has previously worked on special projects for a number of luxury brands.</p>
-
-                    <p>Dedicated to everything pop culture, Laura uses her wealth of knowledge gleaned from vast experience at youth culture titles and her love of research as a springboard for all her forward-facing work — be it editorial, branded content, consulting for brands, research, features writing, trend forecasting case studies or communications.</p>
-                </div>
                 </div>
                 <img
                     src={svgPathThree}
